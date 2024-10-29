106dff5bd38045069634d151e59c7a56be664e84
Nyquist Nwaukwa <nyquist.nwaukwa@gmail.com>
2024-10-29 02:25:26 +0100

    11st11

--- config/swagger.js
+++ config/swagger.js
@@ -25,15 +25,7 @@ const swaggerOptions = {
 const swaggerDocs = swaggerJsDoc(swaggerOptions);
 
 const setupSwagger = (app) => {
-  app.use(
-    "/api-docs",
-    swaggerUi.serve,
-    swaggerUi.setup(swaggerDocs, {
-      customCss:
-        ".swagger-ui .opblock .opblock-summary-path-description-wrapper { align-items: center; display: flex; flex-wrap: wrap; gap: 0 10px; padding: 0 10px; width: 100%; }",
-      customCssUrl: swaggerUICss,
-    })
-  );
+  app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 };
 
 module.exports = setupSwagger;
