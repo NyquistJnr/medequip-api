4442454c1b64951d59c031b43394eaac2530ef93
Kooffreh98 <kooffreh2015@gmail.com>
2024-11-21 13:47:39 +0100

    json limit update

--- api/server.js
+++ api/server.js
@@ -15,8 +15,8 @@ const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json({limit: '10mb'}));
+app.use(bodyParser.urlencoded({ limit: '10mb', extended: true }));
 setupSwagger(app);
 
 app.get("/", (req, res) => {
