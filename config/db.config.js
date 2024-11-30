072055434bfd01675a760ec682892c44c97bcf68
Nyquist Nwaukwa <nyquist.nwaukwa@gmail.com>
2024-11-30 17:57:53 +0100

    1

--- config/db.config.js
+++ config/db.config.js
@@ -8,3 +8,24 @@ const sequelize = new Sequelize(process.env.DATABASE_URL, {
 });
 
 module.exports = sequelize;
+
+/* 
+
+require("dotenv").config();
+const { Sequelize } = require("sequelize");
+
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    dialect: "wolcare",
+    port: process.env.DB_PORT,
+  }
+);
+
+module.exports = sequelize;
+
+
+*/
